ac8aa1c5985668fb1f794e276cccd79e1603d807
Hayder <hayder9701@gmail.com>
2021-09-12 18:45:13 -0400

    Add Sensor repository tests

---
+++ src/test/java/org/traffic/traffic_producer/sensors/SensorMongoRepositoryTest.java
@@ -0,0 +1,83 @@
+package org.traffic.traffic_producer.sensors;
+
+import static org.traffic.traffic_producer.sensors.SensorMongoRepository.COLLECTION_NAME;
+
+import io.vertx.core.Vertx;
+import io.vertx.core.json.JsonArray;
+import io.vertx.core.json.JsonObject;
+import io.vertx.ext.mongo.MongoClient;
+import io.vertx.junit5.VertxExtension;
+import io.vertx.junit5.VertxTestContext;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+import java.util.stream.Collectors;
+import lombok.extern.slf4j.Slf4j;
+import lombok.val;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+
+@ExtendWith(VertxExtension.class)
+@Slf4j
+class SensorMongoRepositoryTest {
+
+  private static SensorMongoRepository repository;
+
+  private static MongoClient mongoClient;
+
+  private static List<Sensor> manySensors;
+
+  @BeforeAll
+  static void configure(Vertx vertx) {
+    JsonObject config =
+        new JsonObject(vertx.fileSystem().readFileBlocking("sensors/sensor-data-source.json"));
+    repository = new SensorMongoRepository(vertx, config);
+    mongoClient = MongoClient.create(vertx, config);
+
+    JsonArray anArrayOfSensors =
+        new JsonObject(vertx.fileSystem().readFileBlocking("sensors/sample-sensors.json"))
+            .getJsonArray("list");
+
+    manySensors =
+        anArrayOfSensors.stream()
+            .map(__ -> ((JsonObject) __))
+            .map(Sensor::fromJson)
+            .collect(Collectors.toList());
+  }
+
+  @AfterAll
+  static void tearDown(VertxTestContext testContext) {
+    mongoClient.dropCollection(COLLECTION_NAME).onComplete(testContext.succeedingThenComplete());
+  }
+
+  @BeforeEach
+  void reset(VertxTestContext testContext) {
+    mongoClient.dropCollection(COLLECTION_NAME).onComplete(testContext.succeedingThenComplete());
+  }
+
+  @Test
+  @DisplayName(
+      "When a request to save a list of sensors is made it must return the same "
+          + " list of sensor succeeding")
+  void saveAllTest(VertxTestContext testContext) throws Throwable {
+    val expected = manySensors;
+    repository
+        .saveAll(expected)
+        .onComplete(
+            testContext.succeeding(
+                result ->
+                    testContext.verify(
+                        () -> {
+                          Assertions.assertEquals(expected, result);
+                          testContext.completeNow();
+                        })));
+
+    Assertions.assertTrue(testContext.awaitCompletion(5, TimeUnit.SECONDS));
+
+    if (testContext.failed()) throw testContext.causeOfFailure();
+  }
+}
