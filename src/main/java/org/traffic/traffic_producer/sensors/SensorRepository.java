5c1c9e335485099735a3b7e303981fe3af3ba259
Hayder <hayder9701@gmail.com>
2021-09-12 18:35:00 -0400

    Add Observation, Sensor and Stream repositories

---
+++ src/main/java/org/traffic/traffic_producer/sensors/SensorRepository.java
@@ -0,0 +1,9 @@
+package org.traffic.traffic_producer.sensors;
+
+import io.vertx.core.Future;
+import java.util.List;
+
+public interface SensorRepository {
+
+  Future<List<Sensor>> saveAll(List<Sensor> sensor);
+}
